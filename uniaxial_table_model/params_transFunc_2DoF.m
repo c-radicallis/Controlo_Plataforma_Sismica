c2613cd09f2479b6cf64bd92836db98f2a7a9bc7
c-radicallis <afonso.henrique@live.com.pt>
2024-12-13 11:09:06 +0000

    commit

--- uniaxial_table_model/params_transFunc_2DoF.m
+++ uniaxial_table_model/params_transFunc_2DoF.m
@@ -34,13 +34,12 @@ zeta2 = 0.1; %
 k2 = m2*(2*pi*f2)^2; %N/m
 c2 = zeta2*2*m2*2*pi*f2; %N/m/s
 
-%% TF's
-
-s=tf('s')
-
 %Controller
 k_p=1.2993/1e-2 %SI units %Pgain (kp=1.2993 V/cm) 
-G_c = tf(k_p,1)    
+G_c = tf(k_p,1)  
+
+%% TF's
+s=tf('s')  
 
 G_T=mT*s^2+[cT+c1]*s+k1
 G_1=m1*s^2+[c1+c2]*s+k1+k2
@@ -60,6 +59,19 @@ G_Fp_xref = G_csv/(k_pl/A+A*s/k_h+G_xT_Fp*(G_csv + A*s))
 G_xT_xref = G_Fp_xref * G_xT_Fp
 G_x1_xref = G_x1_xT*G_xT_xref
 G_x2_xT = G_x2_x1 * G_x1_xT
+
+
+
+%%
+dados = load('elcentro.txt');
+
+t_vector = dados(:,1);
+t_step = t_vector(2)
+ddx = [t_vector dados(:,2)];
+ddy = [t_vector  dados(:,3)];
+
+%plot(ddx(:,1) ,ddx(:,2))
+
 
 %%
 % If axes don't exist, create them
@@ -89,30 +101,25 @@ bode(G_x2_xT);
 title('Bode of G\_x2\_xT'); 
 legend();
 
+ddx_ref=ddx(:,2)
+ddx_T = lsim(G_xT_xref, ddx(:,2) ,ddx(:,1))
+% displacements in milimeters
+x_ref = lsim(1e3/s^2,  ddx(:,2) ,ddx(:,1))
+x_T = lsim(G_xT_xref*1e3/s^2 ,  ddx(:,2) ,ddx(:,1))
+
 % Fourth plot
 axes(ax4); % Activate the existing axes
 hold on
-lsim(G_xT_xref/s^2*1e3 ,  ddx(:,2) ,ddx(:,1),)
+
 hold on
-lsim(1/s^2*1e3,  ddx(:,2) ,ddx(:,1))
-title('Table Displacement(mm)'); 
-legend()
+
+title('Displacement(mm)'); 
+legend('Table','Reference')
 
 
-    
+
 
 %%
-dados = load('elcentro.txt');
+C = pidtune(G_xT_xref,'PID');
 
-t_vector = dados(:,1);
-t_step = t_vector(2)
-ddx = [t_vector dados(:,2)];
-ddy = [t_vector  dados(:,3)];
-
-%plot(ddx(:,1) ,ddx(:,2))
-
-%%
-% figure(3)
-% hold on
-% lsim(G_xT_xref ,  ddx(:,2) ,ddx(:,1))
-% legend()
+pidTuner(G_xT_xref,C)

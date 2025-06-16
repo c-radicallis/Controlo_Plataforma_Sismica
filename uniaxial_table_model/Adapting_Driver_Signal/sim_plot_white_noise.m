69f93ed90d19f74a06a755486a3b85e7a93179cb
c-radicallis <afonso.henrique@live.com.pt>
2025-06-16 21:02:41 +0100

    created writeTXT_then_LTF.m , but now LTF_to_TXT_then_load isn't working

--- uniaxial_table_model/Adapting_Driver_Signal/sim_plot_white_noise.m
+++ uniaxial_table_model/Adapting_Driver_Signal/sim_plot_white_noise.m
@@ -52,17 +52,5 @@ ddx_acq = secondDerivativeTime(x_acq,t_step);
 %%  --- Write .txt file from simulated "acquired" data 
 filename_acq = strrep(newName, '.drv.txt', '_acq.txt');
 save_folder = 'C:\Users\afons\OneDrive - Universidade de Lisboa\Controlo de Plataforma Sismica\uniaxial_table_model\Adapting_Driver_Signal\PRJ_project';
-writeTXT(t_vector , x_acq , ddx_acq , save_folder , filename_acq)
+writeTXT_then_LTF(t_vector , x_acq , ddx_acq , save_folder , filename_acq)
 
-%% Run .txt to .LTF conversion
-
-full_filename_acq = fullfile(save_folder, filename_acq);
-try
-    py_output = py.TXT_to_LTF.txt_to_ltf(full_filename_acq, out_dir);
-    % py_output is a Python string; convert to MATLAB char:
-    output_path = char(py_output);
-    fprintf('Python function returned output path: %s\n', output_path);
-catch ME
-    disp('Error calling Python function:');
-    disp(ME.message);
-end
---
+++ uniaxial_table_model/Adapting_Driver_Signal/writeTXT_then_LTF.m
@@ -0,0 +1,15 @@
+function writeTXT_then_LTF(t_vector , x_acq , ddx_acq , save_folder , filename_acq)
+    
+    writeTXT(t_vector , x_acq , ddx_acq , save_folder , filename_acq)
+    full_filename_acq = fullfile(save_folder, filename_acq);
+    try
+        py_output = py.TXT_to_LTF.txt_to_ltf(full_filename_acq, out_dir);
+        % py_output is a Python string; convert to MATLAB char:
+        output_path = char(py_output);
+        fprintf('Python function returned output path: %s\n', output_path);
+    catch ME
+        disp('Error calling Python function:');
+        disp(ME.message);
+    end
+
+end

bfb08efde7c6d9144c1e9b183429f177e854243f
c-radicallis <afonso.henrique@live.com.pt>
2026-04-23 11:00:10 +0100

    optimized  ResponseSpectrum.m function, made the input arguments for ref_disp and f_vector_disp optional

--- uniaxial_table_model/ResponseSpectrum.m
+++ uniaxial_table_model/ResponseSpectrum.m
@@ -1,26 +1,33 @@
-function [ picos_ddx_m , picos_x_m ] = ResponseSpectrum( t_vector , ref_disp , ref_accel , f_vector , do_displacement )
+function [picos_ddx_m, picos_x_m] = ResponseSpectrum(f_vector_accel, t_vector, ref_accel, ref_disp, f_vector_disp)
+    m    = 1;
+    zeta = 0.05;
 
-    m=1;%1kg
-    zeta=0.05; %damping ratio (%)s
-    
-    picos_ddx_m = zeros( length(f_vector) ,1 );
-    picos_x_m = picos_ddx_m;
-    
-    for i=1:length(f_vector)    
-      
-        % from the desired natural frequency, determining stiffness and damping  
-        k = m*(2*pi*f_vector(i))^2; %N/m
-        c = zeta*2*m*2*pi*f_vector(i); %N/m/s
-          
-        % simulacao do modelo 
-        s=tf('s');
-        ddx_m = lsim( (c*s+k)/(m*s^2+c*s+k) , ref_accel , t_vector ,'zoh'); % Funçao de tranferencia de mola massa amortecedor
-        picos_ddx_m(i)=max(abs( ddx_m(:,1) ));  
-        
-        if do_displacement==1
-            x_m = lsim( (c*s+k)/(m*s^2+c*s+k) , ref_disp , t_vector ,'zoh');
-            picos_x_m(i)=max(abs( x_m(:,1) ));  
+    accel_points = length(f_vector_accel);
+    picos_ddx_m  = zeros(accel_points, 1);
+    picos_x_m    = [];
+
+    do_displacement = nargin == 5;  % both ref_disp and f_vector_disp provided
+
+    if do_displacement
+        picos_x_m = zeros(length(f_vector_disp), 1);
+    end
+
+    dt = t_vector(2) - t_vector(1);
+
+    for i = 1:accel_points
+        k = m * (2*pi*f_vector_accel(i))^2;
+        c = zeta * 2 * m * 2*pi*f_vector_accel(i);
+
+        sys_d  = c2d(tf([c k], [m c k]), dt, 'zoh');
+        [b, a] = tfdata(sys_d, 'v');
+
+        picos_ddx_m(i) = max(abs(filter(b, a, ref_accel)));
+
+        if do_displacement
+            [is_in, idx] = ismember(f_vector_accel(i), f_vector_disp);
+            if is_in
+                picos_x_m(idx) = max(abs(filter(b, a, ref_disp)));
+            end
         end
-
     end
 end
